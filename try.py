c2cbc24602b41f287dac0a7c3985f99c7bfc203b
wzh <337245354@qq.com>
2020-01-29 01:49:00 +0800

    01290148

--- try.py
+++ try.py
@@ -28,31 +28,30 @@ test_loader = torch.utils.data.DataLoader(dataset = test_dataset,
                                         shuffle = True)
 
 digit = train_loader.dataset.train_data[0] #取第一个图片的数据
-plt.imshow(digit,cmap=plt.cm.binary)
+# plt.imshow(digit,cmap=plt.cm.binary)
 # plt.show()
 # print(train_loader.dataset.train_labels[0]) #输出对应的标签
 
+def getXmean(X_train):
+    X_train = np.reshape(X_train, (X_train.shape[0], -1))
+    #将图片从二维展开为一维
+    mean_image = np.mean(X_train, axis=0)
+    #求出训练集中所有图片每个像素位置上的平均值
+    return mean_image
+
+def centralized(X_test,mean_image):
+    X_test = np.reshape(X_test, (X_test.shape[0], -1)) #将图片从二维展开为一维
+    X_test = X_test.astype(np.float)
+    X_test -= mean_image #减去均值图像，实现零均值化
+    return X_test
+
 if __name__ == '__main__':
-
-    X_train = train_loader.dataset.train_data.numpy() #需要转为numpy矩阵
-    X_train = X_train.reshape(X_train.shape[0],28*28)#需要reshape之后才能放入knn分类器
-    y_train = train_loader.dataset.train_labels.numpy()
-    X_test = test_loader.dataset.test_data[:1000].numpy()
-    X_test = X_test.reshape(X_test.shape[0],28*28)
-    y_test = test_loader.dataset.test_labels[:1000].numpy()
-    num_test = y_test.shape[0]
-    y_test_pred = knn.kNN_classify(5, 'M', X_train, y_train, X_test)
-    num_correct = np.sum(y_test_pred == y_test)
-    accuracy = float(num_correct) / num_test
-    print('Got %d / %d correct => accuracy: %f' % (num_correct, num_test, accuracy))
-
-    # # 使用预处理来完成knn
-    # X_train = train_loader.dataset.train_data.numpy()
-    # mean_image = getXmean(X_train)
-    # X_train = centralized(X_train, mean_image)
-    # y_train = train_loader.dataset.train_labels.numpy()
+    #
+    # X_train = train_loader.dataset.train_data.numpy() #需要转为numpy矩阵
+    # X_train = X_train.reshape(X_train.shape[0],28*28)#需要reshape之后才能放入knn分类器,需要训练的图矩阵
+    # y_train = train_loader.dataset.train_labels.numpy()#训练的label
     # X_test = test_loader.dataset.test_data[:1000].numpy()
-    # X_test = centralized(X_test, mean_image)
+    # X_test = X_test.reshape(X_test.shape[0],28*28)
     # y_test = test_loader.dataset.test_labels[:1000].numpy()
     # num_test = y_test.shape[0]
     # y_test_pred = knn.kNN_classify(5, 'M', X_train, y_train, X_test)
@@ -60,6 +59,17 @@ if __name__ == '__main__':
     # accuracy = float(num_correct) / num_test
     # print('Got %d / %d correct => accuracy: %f' % (num_correct, num_test, accuracy))
 
-
-
+    # 使用预处理来完成knn
+    X_train = train_loader.dataset.train_data.numpy()
+    mean_image = getXmean(X_train)
+    X_train = centralized(X_train, mean_image)
+    y_train = train_loader.dataset.train_labels.numpy()
+    X_test = test_loader.dataset.test_data[:1000].numpy()
+    X_test = centralized(X_test, mean_image)
+    y_test = test_loader.dataset.test_labels[:1000].numpy()
+    num_test = y_test.shape[0]
+    y_test_pred = knn.kNN_classify(5, 'M', X_train, y_train, X_test)            #E--96%    / M--0.087%
+    num_correct = np.sum(y_test_pred == y_test)
+    accuracy = float(num_correct) / num_test
+    print('Got %d / %d correct => accuracy: %f' % (num_correct, num_test, accuracy))
 

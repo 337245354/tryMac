deae7e516f2dd6806adaaea79a0a73e41f46f97e
wzh <337245354@qq.com>
2020-01-28 15:44:20 +0800

    01281543

--- try.py
+++ try.py
@@ -1,8 +1,13 @@
+import matplotlib
 import torch
 from torch.utils.data import DataLoader
 import torchvision.datasets as dsets
 import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.pyplot as plt
+import operator
+import knn
 
 batch_size = 100
 # MNIST dataset
@@ -21,14 +26,40 @@ train_loader = torch.utils.data.DataLoader(dataset = train_dataset,
 test_loader = torch.utils.data.DataLoader(dataset = test_dataset,
                                         batch_size = batch_size,
                                         shuffle = True)
-# print("train_data:", train_dataset.train_data.size())
-# print("train_labels:", train_dataset.train_labels.size())
-# print("test_data:", test_dataset.test_data.size())
-# print("test_labels:", test_dataset.test_labels.size())
 
 digit = train_loader.dataset.train_data[0] #取第一个图片的数据
 plt.imshow(digit,cmap=plt.cm.binary)
-plt.show()
+# plt.show()
 # print(train_loader.dataset.train_labels[0]) #输出对应的标签
+
+if __name__ == '__main__':
+
+    X_train = train_loader.dataset.train_data.numpy() #需要转为numpy矩阵
+    X_train = X_train.reshape(X_train.shape[0],28*28)#需要reshape之后才能放入knn分类器
+    y_train = train_loader.dataset.train_labels.numpy()
+    X_test = test_loader.dataset.test_data[:1000].numpy()
+    X_test = X_test.reshape(X_test.shape[0],28*28)
+    y_test = test_loader.dataset.test_labels[:1000].numpy()
+    num_test = y_test.shape[0]
+    y_test_pred = knn.kNN_classify(5, 'M', X_train, y_train, X_test)
+    num_correct = np.sum(y_test_pred == y_test)
+    accuracy = float(num_correct) / num_test
+    print('Got %d / %d correct => accuracy: %f' % (num_correct, num_test, accuracy))
+
+    # # 使用预处理来完成knn
+    # X_train = train_loader.dataset.train_data.numpy()
+    # mean_image = getXmean(X_train)
+    # X_train = centralized(X_train, mean_image)
+    # y_train = train_loader.dataset.train_labels.numpy()
+    # X_test = test_loader.dataset.test_data[:1000].numpy()
+    # X_test = centralized(X_test, mean_image)
+    # y_test = test_loader.dataset.test_labels[:1000].numpy()
+    # num_test = y_test.shape[0]
+    # y_test_pred = knn.kNN_classify(5, 'M', X_train, y_train, X_test)
+    # num_correct = np.sum(y_test_pred == y_test)
+    # accuracy = float(num_correct) / num_test
+    # print('Got %d / %d correct => accuracy: %f' % (num_correct, num_test, accuracy))
+
+
 
 

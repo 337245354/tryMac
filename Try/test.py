abadfb8463a60ba768e6afc98706cfbb45b322fa
wzh <337245354@qq.com>
2020-02-12 00:38:56 +0800

    02121238

--- Try/test.py
+++ Try/test.py
@@ -7,4 +7,11 @@ def bar():
 
 
 
-foo()
+
+
+import numpy as np
+arr1 = np.arange(0,30,5).reshape(2,3)
+it = np.nditer(arr1, flags=['multi_index'],op_flags=['readwrite'])
+while not it.finished:
+    print(it.multi_index)
+    it.iternext()
